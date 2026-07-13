54977779d0d60cc5bec13ce6846e19770ab85fcf
LSanti94 <luissantillan1994@hotmail.com>
2026-07-12 21:07:25 -0500

    auth-facebook

--- app/src/main/java/com/example/ch3mxr/base/MainActivity.kt
+++ app/src/main/java/com/example/ch3mxr/base/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.ch3mxr.ui.features.App
 import com.example.ch3mxr.ui.theme.Ch3mxrTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
